187ae9fea8b760cc0577dbfa2a12afd8ba7535ba
Elaviers <elaviers@gmail.com>
2021-02-16 00:09:19 -0800

    Initial commit

---
+++ TestFile.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+template <typename T>
+class PooClass
+{
+	T* ptr;
+
+	void add()
+	{
+		*ptr += 3;
+	}
+};
+
+int main(char* argsv[], int argsc)
+{
+	int ints[30] = { 1, 2, 3 };
+
+	for (int i = 0; i < 30; ++i)
+		ints[i] = 30 - ints[i] * 2;
+
+	char* str = "Test string\n";
+
+	char* raw = R"(
+	THIS
+	IS A
+	RAW STRING)";
+
+	struct poop
+	{
+		int a, b;
+	};
+
+	return 0;
+}
---
+++ TestFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElaviersTheme
+{
+    class TestFile
+    {
+        public float Good { get { return Good; } set{ Good = value; } }
+
+        public static void Test(int poop, int wee)
+        {
+            poop += 3;
+
+
+        }
+
+    }
+}
