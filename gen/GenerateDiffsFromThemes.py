92542529a65855aa1ea945c1fd001063b0bb1f7f
Elaviers <elaviers@gmail.com>
2021-08-22 10:06:52 -0700

    Update green theme

--- gen/GenerateDiffsFromThemes.py
+++ gen/GenerateDiffsFromThemes.py
@@ -1,80 +1,92 @@
 import os
 import xml.etree.ElementTree as etree
+from typing import List
 
-diffOut = 'ThemeDiff.xml'
-diffFrom = 'Dark.xml'
-diffTo = 'Streamlined.vstheme'
+def genDiffFile(diffFrom: str, diffTo: str, diffOut: str, themes: List[tuple]):
+	filedir = os.path.dirname(__file__)
+	basefile = os.path.join(filedir, diffFrom)
+	baseroot = etree.parse(basefile).getroot()
+	basetheme = baseroot.find('Theme')
 
-themes = [
-	('Streamlined.vstheme', 'Dark.xml'),
-	('StreamlinedGreen.vstheme', 'DarkGreen.xml'),
-]
+	themefile = os.path.join(filedir, diffTo)
+	themeroot = etree.parse(themefile).getroot()
+	theme = themeroot.find('Theme')
+
+	outfile = os.path.join(filedir, diffOut)
+	outDiff = etree.Element('ThemeDiff')
+	outroot = etree.ElementTree(outDiff)
+
+	for reftheme in themes:
+		themeref = etree.SubElement(outDiff, 'Theme')
+		themeref.attrib = etree.parse(os.path.join(filedir, reftheme[0])).find('Theme').attrib
+		themeref.set('Input', reftheme[1])
+		themeref.set('Output', reftheme[0])
+
+		for i in range(2, len(reftheme)):
+			etree.SubElement(themeref, 'Run').text = reftheme[i]
+
+	categories = theme.findall('Category')
+	for category in categories:
+		guid = category.get('GUID')
+		basecategory = basetheme.find(f'Category[@GUID=\'{guid}\']')
+		outCategory = None
+
+		if basecategory != None:
+			colours = category.findall('Color')
+			for colour in colours:
+				name = colour.get('Name')
+				basecolour = basecategory.find(f'Color[@Name=\'{name}\']')
+
+				skip1 = skip2 = True
+				fg = colour.find('Foreground')
+				if fg != None:
+					fg = fg.get('Source')
+
+					if basecolour != None:
+						basefg = basecolour.find('Foreground')
+						skip1 = basefg != None and basefg.get('Source') == fg
+
+				bg = colour.find('Background')
+				if bg != None:
+					bg = bg.get('Source')
+
+					if basecolour != None:
+						basebg = basecolour.find('Background')
+						skip2 = basebg != None and basebg.get('Source') == bg
+				
+				if basecolour == None:
+					print(f'Error: Colour \"{name}\" is not in base!')
+				elif skip1 and skip2:
+					continue
+
+				#colour is changed, add it to our thing
+				if outCategory == None:
+					outCategory = etree.SubElement(outDiff, 'Category', {'Name': category.get('Name'), 'GUID': guid})
+
+				item = etree.SubElement(outCategory, 'Item', {'Name': name })
+				if fg != None: item.set('Foreground', fg)
+				if bg != None: item.set('Background', bg)
+
+				print(name)
+
+	outroot.write(outfile)
 
 ############
 ############
 ############
+############
+############
+############
 
-filedir = os.path.dirname(__file__)
-basefile = os.path.join(filedir, diffFrom)
-baseroot = etree.parse(basefile).getroot()
-basetheme = baseroot.find('Theme')
+genDiffFile('base/Dark.xml', 'Streamlined.vstheme', 'diff/ThemeDiff.xml', [
+	('Streamlined.vstheme', 'base/Dark.xml'),
+	('StreamlinedGreen.vstheme', 'base/Dark.xml', 'diff/GreenDiff.xml', 'diff/GreenDiff_CommonElements.xml')
+])
 
-themefile = os.path.join(filedir, diffTo)
-themeroot = etree.parse(themefile).getroot()
-theme = themeroot.find('Theme')
+#
+# Regens GreenDiff.xml
+#  vvvv
+#genDiffFile('base/Dark.xml', 'base/DarkGreen.xml', 'diff/GreenDiff.xml', [])
+#
 
-outfile = os.path.join(filedir, diffOut)
-outDiff = etree.Element('ThemeDiff')
-outroot = etree.ElementTree(outDiff)
-
-for reftheme in themes:
-	themeref = etree.SubElement(outDiff, 'Theme')
-	themeref.attrib = etree.parse(os.path.join(filedir, reftheme[0])).find('Theme').attrib
-	themeref.set('Input', reftheme[1])
-	themeref.set('Output', reftheme[0])
-
-categories = theme.findall('Category')
-for category in categories:
-	guid = category.get('GUID')
-	basecategory = basetheme.find(f'Category[@GUID=\'{guid}\']')
-	outCategory = None
-
-	if basecategory != None:
-		colours = category.findall('Color')
-		for colour in colours:
-			name = colour.get('Name')
-			basecolour = basecategory.find(f'Color[@Name=\'{name}\']')
-
-			skip1 = skip2 = True
-			fg = colour.find('Foreground')
-			if fg != None:
-				fg = fg.get('Source')
-
-				if basecolour != None:
-					basefg = basecolour.find('Foreground')
-					skip1 = basefg != None and basefg.get('Source') == fg
-
-			bg = colour.find('Background')
-			if bg != None:
-				bg = bg.get('Source')
-
-				if basecolour != None:
-					basebg = basecolour.find('Background')
-					skip2 = basebg != None and basebg.get('Source') == bg
-			
-			if basecolour == None:
-				print(f'Error: Colour \"{name}\" is not in base!')
-			elif skip1 and skip2:
-				continue
-
-			#colour is changed, add it to our thing
-			if outCategory == None:
-				outCategory = etree.SubElement(outDiff, 'Category', {'Name': category.get('Name'), 'GUID': guid})
-
-			item = etree.SubElement(outCategory, 'Item', {'Name': name })
-			if fg != None: item.set('Foreground', fg)
-			if bg != None: item.set('Background', bg)
-
-			print(name)
-
-outroot.write(outfile)
+genDiffFile('base/Dark.xml', 'base/DarkGreen_CommonElements.xml', 'diff/GreenDiff_CommonElements.xml', [])
--- gen/GenerateThemesFromDiffs.py
+++ gen/GenerateThemesFromDiffs.py
@@ -1,29 +1,15 @@
 import copy
 import os
 import xml.etree.ElementTree as etree
+from typing import List
 
 filedir = os.path.dirname(__file__)
 
-dfile = os.path.join(filedir, os.path.join(filedir, 'ThemeDiff.xml'))
-dtree = etree.parse(dfile)
-themes = dtree.findall('Theme')
-
-for theme in themes:
-    basefile = os.path.join(filedir, theme.get('Input'))
-    basetree = etree.parse(basefile)
-
-    outfile = os.path.join(filedir, theme.get('Output'))
-    out = copy.deepcopy(basetree)
-
-    otheme = out.find('Theme')
-    otheme.set('Name', theme.get('Name'))
-    otheme.set('GUID', theme.get('GUID'))
-    otheme.set('BaseGUID', theme.get('BaseGUID'))
-    
-    categories = out.findall('.//Category')
+def applyDiffToCategories(diff: etree.ElementTree, dest: etree.ElementTree):
+    categories = dest.findall('.//Category')
     for category in categories:
         guid = category.get('GUID')
-        dcategory = dtree.find(f'.//Category[@GUID=\'{guid}\']')
+        dcategory = diff.find(f'.//Category[@GUID=\'{guid}\']')
         if dcategory != None:
             colours = category.findall('Color')
             for colour in colours:
@@ -38,4 +24,29 @@ for theme in themes:
 
                     print(name)
 
-    out.write(outfile)
+def GenThemeFile(diffFile: str):
+    dfile = os.path.join(filedir, os.path.join(filedir, diffFile))
+    dtree = etree.parse(dfile)
+    themes = dtree.findall('Theme')
+
+    for theme in themes:
+        basefile = os.path.join(filedir, theme.get('Input'))
+        basetree = etree.parse(basefile)
+
+        outfile = os.path.join(filedir, theme.get('Output'))
+        out = copy.deepcopy(basetree)
+
+        otheme = out.find('Theme')
+        otheme.set('Name', theme.get('Name'))
+        otheme.set('GUID', theme.get('GUID'))
+        otheme.set('BaseGUID', theme.get('BaseGUID'))
+        
+        runs = theme.findall('Run')
+        for run in runs:
+            tree = etree.parse(os.path.join(filedir, run.text))
+            applyDiffToCategories(tree, out)
+
+        applyDiffToCategories(dtree, out)
+        out.write(outfile)
+
+GenThemeFile('diff/ThemeDiff.xml')
